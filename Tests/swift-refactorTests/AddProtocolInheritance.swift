3d7f735b62b27fe7a85040a483543fb379ca0c8e
Aleksey Yakimenko <aleksey.yakimenko@agoda.com>
2022-08-20 14:00:19 +0700

    Add option to modify extensions

--- Tests/swift-refactorTests/AddProtocolInheritance.swift
+++ Tests/swift-refactorTests/AddProtocolInheritance.swift
@@ -25,6 +25,13 @@ final class AddProtocolInheritanceTests: XCTestCase {
             expected: "enum Enum: Encodable, Equatable, MyProtocol {}"
         )
     }
+    
+    func testEnum_WithFewProtocols_WithMyProtocol() {
+        assert(
+            input: "enum Enum: Encodable, Equatable, MyProtocol {}",
+            expected: "enum Enum: Encodable, Equatable, MyProtocol {}"
+        )
+    }
 
     func testProtocol_NoInheritance() {
         assert(
@@ -39,13 +46,76 @@ final class AddProtocolInheritanceTests: XCTestCase {
             expected: "protocol MyProtocol: Encodable, Equatable {}"
         )
     }
+    
+    func testStruct_WithRewritableProtocol_WithTrailingSpaces() {
+        assert(
+            input: "struct A: MyProtocol     {}",
+            expected: "struct A: MyProtocol {}"
+        )
+    }
+    
+    func testStruct_WithRewritableProtocol_WithConflictingTypes() {
+        assert(
+            input: "struct A: MyProtocol, MyProtocol_A, MyProtocol_B {}",
+            expected: "struct A: MyProtocol {}"
+        )
+    }
+    
+    func testStruct_WithRewritableProtocol_WithExtension() {
+        assert(
+            input: """
+                struct A {}
+                extension A: MyProtocol {}
+                """,
+            expected: """
+                struct A: MyProtocol {}
+                """
+        )
+    }
+    
+    func testStruct_WithExtensionWithoutProtocol() {
+        assert(
+            input: """
+                struct A {}
+                extension A {}
+                """,
+            expected: """
+                struct A: MyProtocol {}
+                extension A {}
+                """
+        )
+    }
+    
+    func testStruct_WithExtensionWithNotExcludedProtocol() {
+        assert(
+            input: """
+                struct A {}
+                extension A: ShouldNotBeRemovedProtocol {}
+                """,
+            expected: """
+                struct A: MyProtocol {}
+                extension A: ShouldNotBeRemovedProtocol {}
+                """
+        )
+    }
+    
+    func testStruct_WithFewProtocols_WithMyProtocol() {
+            assert(
+                input: """
+                    struct A: AnyProtocol, MyProtocol {}
+                    """,
+                expected: """
+                    struct A: AnyProtocol, MyProtocol {}
+                    """
+        )
+    }
 }
 
 extension AddProtocolInheritanceTests {
     func assert(input: String, expected: String) {
         do {
             let rootNode: SourceFileSyntax = try SyntaxParser.parse(source: input)
-            let result = AddProtocolInheritance().visit(rootNode)
+            let result = AddProtocolInheritance("MyProtocol", ["MyProtocol_A", "MyProtocol_B"]).visit(rootNode)
             XCTAssertEqual(expected, result.description)
         } catch {
             XCTFail(error.localizedDescription)
