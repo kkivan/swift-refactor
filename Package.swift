59e00f6814f648ecc8e0bfc7985d6467835603a5
Ivan Kvyatkovskiy <ivan.kvyatkovskiy@agoda.com>
2023-05-05 17:38:57 +0700

    update swift syntax

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git",
-                 .exact("0.50600.1"))
+                 .exact("0.50700.1"))
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -20,6 +20,7 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
             ]),
         .testTarget(
             name: "swift-refactorTests",
@@ -27,6 +28,7 @@ let package = Package(
                 "swift-refactor",
                 .product(name: "SwiftSyntax", package: "swift-syntax"),
                 .product(name: "SwiftSyntaxParser", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
             ]),
     ]
 )
